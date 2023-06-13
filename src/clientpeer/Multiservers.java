6b32107a0bfe3ac95716410996a25475be729678
RafaZamoraUPIITA <99270205+RafaZamoraUPIITA@users.noreply.github.com>
2023-06-13 01:57:31 -0600

    Proyecto final del lado del cliente

---
+++ src/clientpeer/Multiservers.java
@@ -0,0 +1,48 @@
+package clientpeer;
+
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author alonsobernal
+ */
+public class Multiservers extends Thread {
+
+    static int NoClients = 0;
+    static int NoRegistro = 1;
+
+    public void run() {
+        ServerSocket socketServidor = null;
+        Socket socketCliente = null;
+
+        try {
+            socketServidor = new ServerSocket(8080);
+        } catch (IOException e) {
+            System.out.println("Multiservers: ERROR 2- " + e.toString());
+            System.exit(0);
+        }
+
+        System.out.println("\t**Iniciado servidor PEER**");
+        int enproceso = 1;
+        while (enproceso == 1) {
+            try {
+                socketCliente = socketServidor.accept();
+                MultiServerThread controlThread = new MultiServerThread(socketCliente);
+                controlThread.start();
+            } catch (IOException e) {
+                System.out.println("Multiservers: ERROR 2- " + e.toString());
+                try {
+                    socketServidor.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Multiservers.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                System.exit(0);
+            }
+        }
+        System.out.println("Finalizando Servidor...");
+
+    }
+}
